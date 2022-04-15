3c72935223ff13ef04812b9ecd49c6992db8890f
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2022-04-15 19:52:52 +0500

    Update func.py

--- func.py
+++ func.py
@@ -617,7 +617,7 @@ def adm(update, context):
        context.bot.send_photo(photo=open('{}.jpeg'.format(user_id), 'rb'), chat_id=-772939946)
        context.bot.send_photo(photo=open('{}.jpeg'.format(user_id+1), 'rb'), chat_id=-772939946)
        context.bot.send_photo(photo=open('Picture.jpeg', 'rb'), chat_id=-772939946, caption=anketa)
-       context.bot.send_message(chat_id=-772939946, text=user_name)
+       context.bot.send_message(chat_id=-772939946, text='@'+user_name)
        context.bot.send_message(chat_id=user_id, text=dct[lang_][37])
        cur.execute("""DELETE FROM Users WHERE TG_ID = "{}" """.format(user_id))
        connect.commit()
